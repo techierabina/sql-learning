a15b7a24045e40c1433cac3bf1ec47c6ce3d7900
Rabina Karki <techierabina@gmail.com>
2025-08-14 16:27:34 -0500

    delete

---
+++ delete.sql
@@ -0,0 +1,17 @@
+-- DELETE
+
+/* Delete all customers with an ID greater than 5*/
+
+DELETE FROM customers
+WHERE id >5
+
+SELECT * 
+FROM customers
+
+/*Delete all data from the table persons*/
+DELETE FROM persons
+
+SELECT * 
+FROM persons
+
+TRUNCATE TABLE 
